6a27aa2d0da4d4317fab320431545c4b7c7030e1
Kazuya Iriguchi <kazuya.iriguchi.j6k@jp.denso.com>
2023-10-02 17:40:02 +0900

    fix: top page

--- 00_🔝_top.py
+++ 00_🔝_top.py
@@ -1,4 +1,26 @@
 import streamlit as st
+import re
 
+# README.md ファイルを開く
+with open("README.md", "r", encoding="utf-8") as file:
+    contents = file.read()
 
-st.title("PDF Editor")
+# Markdownの画像リンクの正規表現パターン
+image_pattern = re.compile(r"!\[.*?\]\((.*?)\)")
+
+# README.mdの内容を行ごとに処理
+for line in contents.split("\n"):
+    # 行内の画像リンクを探す
+    match = image_pattern.search(line)
+    if match:
+        # st.imageを使用して画像を表示
+        image_path = match.group(1)
+        st.image(image_path)
+
+        # 画像リンクを削除して、残りのテキストを表示
+        line = image_pattern.sub("", line)
+        if line.strip():
+            st.markdown(line)
+    else:
+        # 画像リンクがない場合、行をそのまま表示
+        st.markdown(line)
