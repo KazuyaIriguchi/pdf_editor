97039273d3671b89abe6835bfdc6f8c82e25849b
Kazuya Iriguchi <kazuya.iriguchi.j6k@jp.denso.com>
2023-10-03 15:30:39 +0900

    add: docstring

--- functions/sort.py
+++ functions/sort.py
@@ -4,6 +4,15 @@ from pypdf import PdfReader, PdfWriter
 
 
 def rearrange_pdf(pdf_bytes: io.BytesIO, order: list) -> io.BytesIO:
+    """PDFを並び替えて返却する
+
+    Args:
+        pdf_bytes (io.BytesIO): PDFのバイナリデータ
+        order (list): 並び順
+
+    Returns:
+        io.BytesIO: バイナリデータ
+    """
     pdf_reader = PdfReader(pdf_bytes)
     pdf_writer = PdfWriter()
 
